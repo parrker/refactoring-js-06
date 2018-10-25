9f7060840d9c0e6c0f172f61c6d61fdf7b9d1978
Roman Volkov <parrker9@gmail.com>
2018-10-25 23:17:41 +0800

    Remove unnecessary code path

--- nb.js
+++ nb.js
@@ -89,9 +89,7 @@ function classify(chords){
     var first = labelProbabilities[difficulty] + 1.01;
     chords.forEach(function(chord){
       var probabilityOfChordInLabel = probabilityOfChordsInLabels[difficulty][chord];
-      if (probabilityOfChordInLabel === undefined) {
-        first + 1.01;
-      } else {
+      if (probabilityOfChordInLabel !== undefined) {
         first = first * (probabilityOfChordInLabel + 1.01);
       }
     });
