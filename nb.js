36df260d792beafa7d9ae766e4682456b1209e13
Roman Volkov <parrker9@gmail.com>
2018-10-29 21:56:59 +0800

    Remove dead code

--- nb.js
+++ nb.js
@@ -15,7 +15,6 @@ toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7',
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 
 var songs = [];
-var labels = [];
 var allChords = new Set();
 var labelCounts = {};
 var labelProbabilities = {};
@@ -24,7 +23,6 @@ var probabilityOfChordsInLabels = {};
 
 function train(chords, label){
   songs.push([label, chords]);
-  labels.push(label);
   chords.forEach(chord => allChords.add(chord));
   if(Object.keys(labelCounts).includes(label)){
     labelCounts[label] = labelCounts[label] + 1;
