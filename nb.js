41a28bcbbf7f9887c3982ced75806d1a2d73d6e1
Roman Volkov <parrker9@gmail.com>
2018-10-30 21:54:21 +0800

    Add tests, remove console.log and classify calls from the file

--- nb.js
+++ nb.js
@@ -89,8 +89,22 @@ function classify(chords){
     });
     classified.set(difficulty, first);
   });
-  console.log(classified);
+  return classified;
 };
 
-classify(['d', 'g', 'e', 'dm']);
-classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
+var wish = require('wish');
+describe('the file', function () {
+  it('classifies', function () {
+    var classified = classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
+    wish(classified.get(easy) === 1.3433333333333333);
+    wish(classified.get(medium) === 1.5060259259259259);
+    wish(classified.get(hard) === 1.6884223991769547);
+  });
+
+  it('classifies again', function () {
+    var classified = classify(['d', 'g', 'e', 'dm']);
+    wish(classified.get(easy) === 2.023094827160494);
+    wish(classified.get(medium) === 1.855758613168724);
+    wish(classified.get(hard) === 1.855758613168724);
+  });
+});
