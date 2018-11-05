eea0b3b665e6dc2ed5a863911d2267ef3c5eaf3d
Roman Volkov <roman.volkov@biqmind.com>
2018-11-05 19:08:28 +0800

    Extract difficulties into songList

--- nb.js
+++ nb.js
@@ -1,7 +1,8 @@
 var songList = {
+  difficulties: ['easy', 'medium', 'hard'],
   songs: [],
   addSong: function(name, chords, difficulty) {
-    this.songs.push({ name, chords, difficulty });
+    this.songs.push({ name, chords, difficulty: this.difficulties[difficulty] });
   },
 };
 
@@ -14,22 +15,16 @@ var classifier = {
   probabilityOfChordsInLabels: new Map(),
 };
 
-function setDifficulties() {
-  easy = 'easy';
-  medium = 'medium';
-  hard = 'hard';
-}
-
 function setSongs() {
-  songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], easy);
-  songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], easy);
-  songList.addSong('tooManyCooks', ['c', 'g', 'f'], easy);
-  songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], medium);
-  songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], medium);
-  songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'eb', 'fm', 'ab'], medium);
-  songList.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], hard);
-  songList.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], hard);
-  songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], hard);
+  songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
+  songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0);
+  songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
+  songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
+  songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
+  songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'eb', 'fm', 'ab'], 1);
+  songList.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], 2);
+  songList.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
+  songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
 }
 
 function train(chords, label){
@@ -79,7 +74,6 @@ function setLabelsAndProbabilities() {
 }
 
 function trainAll() {
-  setDifficulties();
   setSongs();
   songList.songs.forEach(function(song) {
     train(song.chords, song.difficulty);
@@ -109,21 +103,21 @@ describe('the file', function () {
 
   it('classifies', function () {
     var classified = classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
-    wish(classified.get(easy) === 1.3433333333333333);
-    wish(classified.get(medium) === 1.5060259259259259);
-    wish(classified.get(hard) === 1.6884223991769547);
+    wish(classified.get('easy') === 1.3433333333333333);
+    wish(classified.get('medium') === 1.5060259259259259);
+    wish(classified.get('hard') === 1.6884223991769547);
   });
 
   it('classifies again', function () {
     var classified = classify(['d', 'g', 'e', 'dm']);
-    wish(classified.get(easy) === 2.023094827160494);
-    wish(classified.get(medium) === 1.855758613168724);
-    wish(classified.get(hard) === 1.855758613168724);
+    wish(classified.get('easy') === 2.023094827160494);
+    wish(classified.get('medium') === 1.855758613168724);
+    wish(classified.get('hard') === 1.855758613168724);
   });
 
   it('labelProbabilities', function () {
-    wish(classifier.labelProbabilities.get(easy) === 0.3333333333333333);
-    wish(classifier.labelProbabilities.get(medium) === 0.3333333333333333);
-    wish(classifier.labelProbabilities.get(hard) === 0.3333333333333333);
+    wish(classifier.labelProbabilities.get('easy') === 0.3333333333333333);
+    wish(classifier.labelProbabilities.get('medium') === 0.3333333333333333);
+    wish(classifier.labelProbabilities.get('hard') === 0.3333333333333333);
   });
 });
