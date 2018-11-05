6c75f3819d3f32de380fd6a435716fdd23b26284
Roman Volkov <roman.volkov@biqmind.com>
2018-11-05 19:10:22 +0800

    Move songs to the test suite

--- nb.js
+++ nb.js
@@ -15,18 +15,6 @@ var classifier = {
   probabilityOfChordsInLabels: new Map(),
 };
 
-function setSongs() {
-  songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
-  songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0);
-  songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
-  songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
-  songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
-  songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'eb', 'fm', 'ab'], 1);
-  songList.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], 2);
-  songList.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
-  songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
-}
-
 function train(chords, label){
   classifier.songs.push({ label, chords });
   chords.forEach(chord => classifier.allChords.add(chord));
@@ -74,7 +62,6 @@ function setLabelsAndProbabilities() {
 }
 
 function trainAll() {
-  setSongs();
   songList.songs.forEach(function(song) {
     train(song.chords, song.difficulty);
   });
@@ -99,6 +86,16 @@ function classify(chords){
 
 var wish = require('wish');
 describe('the file', function () {
+  songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
+  songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0);
+  songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
+  songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
+  songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
+  songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'eb', 'fm', 'ab'], 1);
+  songList.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], 2);
+  songList.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
+  songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
+
   trainAll();
 
   it('classifies', function () {
